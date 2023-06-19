b92f4b36eb5bdac5af13eafdea56120056270791
Szarvas Péter <peterszarvas94@gmail.com>
2023-06-19 22:11:54 +0200

    remove: unused files

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,8 +2,6 @@ import { migrationRouter } from "~/server/api/routers/migration";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { usersRouter } from "./routers/users";
 import { accountsRouter } from "./routers/accounts";
-import { incomesRouter } from "./routers/incomes";
-import { accountAdminsRouter } from "./routers/accountadmins";
 
 /**
  * This is the primary router for your server.
@@ -14,8 +12,6 @@ export const appRouter = createTRPCRouter({
   migration: migrationRouter,
   users: usersRouter,
   accounts: accountsRouter,
-  accountAdmins: accountAdminsRouter,
-  incomes: incomesRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/accounts.ts
+++ src/server/api/routers/accounts.ts
@@ -2,7 +2,7 @@ import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { db } from "~/server/db/db";
-import { accounts, accountAdmins } from "~/server/db/schema";
+import { accounts } from "~/server/db/schema";
 
 export const accountsRouter = createTRPCRouter({
   createAccount: publicProcedure
