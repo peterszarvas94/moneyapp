ac5fd43a425fdf68c294bc8c5aa9586abc814ed0
Szarvas Péter <peterszarvas94@gmail.com>
2023-07-07 11:09:17 +0200

    add: account admin to shcema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { toast } from "react-hot-toast";
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
 
   const { signOut, isSignedIn } = useAuth();
-  const { mutateAsync: newUser } = api.user.new.useMutation();
-  const { mutateAsync: getUser } = api.user.getFromId.useMutation();
 
   return (
     <>
@@ -49,38 +46,6 @@ const Home: NextPage = () => {
               Sign in
             </Link>
           )}
-          <button
-            className='underline'
-            onClick={async () => {
-              try {
-                const res = await newUser({
-                  name: "Test User",
-                  email: "test@email.com",
-                  clerkId: "ck_test",
-                })
-                toast.success(`User is created: ${res.name}`);
-              } catch {
-                toast.error(`Can not create user`);
-              }
-            }}
-          >
-            New User
-          </button>
-          <button
-            className='underline'
-            onClick={async () => {
-              try {
-                const res = await getUser({
-                  id: 14
-                })
-                toast.success(`User name is: ${res.name}`);
-              } catch {
-                toast.error("User 14 is not found");
-              }
-            }}
-          >
-            Get User 14
-          </button>
         </div>
       </main>
     </>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,46 @@
-import { InferModel } from 'drizzle-orm';
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { InferModel, relations } from 'drizzle-orm';
+import { integer, primaryKey, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('users', {
   id: integer('id').primaryKey(),
   name: text('name'),
   email: text('email'),
-  clerkId: text('clerkID')
+  clerkId: text('clerk_id')
 })
 
 export type NewUser = InferModel<typeof users, 'insert'>
+
+export const userRelations = relations(users, ({ many }) => ({
+  accountAdmins: many(accountAdmins),
+}))
+
+export const account = sqliteTable('accounts', {
+  id: integer('id').primaryKey(),
+  name: text('name'),
+  description: text('description'),
+})
+
+export type NewAccount = InferModel<typeof account, 'insert'>
+
+export const accountRelations = relations(account, ({ many }) => ({
+  accountAdmins: many(accountAdmins),
+}))
+
+export const accountAdmins = sqliteTable('account_admins', {
+  adminId: integer('admin_id').notNull().references(() => users.id),
+  accountId: integer('account_id').notNull().references(() => account.id),
+}, (t) => ({
+    pk: primaryKey(t.adminId, t.accountId)
+  })
+)
+
+export const accountAdminsRelations = relations(accountAdmins, ({ one }) => ({
+  account: one(account, {
+    fields: [accountAdmins.accountId],
+    references: [account.id],
+  }),
+  admin: one(users, {
+    fields: [accountAdmins.adminId],
+    references: [users.id],
+  }),
+}))
